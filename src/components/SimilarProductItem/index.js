b299d829f65709e24ee45e54c855b3ef1bcac853
raghavendra-07 <raghavendrachary010@gmail.com>
2023-12-02 18:41:12 +0000

    solution

---
+++ src/components/SimilarProductItem/index.js
@@ -0,0 +1,31 @@
+// Write your code here
+import './index.css'
+
+const SimilarProductItem = props => {
+  const {productDetails} = props
+  const {title, brand, imageUrl, rating, price} = productDetails
+  return (
+    <li className="similar-product-item">
+      <img
+        className="similar-product-image"
+        src={imageUrl}
+        alt={`similar product ${title}`}
+      />
+      <p className="similar-product-title">{title}</p>
+      <p className="similar-product-brand">by {brand}</p>
+      <div className="similar-product-price-rating-container">
+        <p className="similar-product-price">Rs {price}</p>
+        <div className="similar-product-rating-container">
+          <p className="similar-product-rating">{rating}</p>
+          <img
+            className="similar-product-star"
+            src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+            alt="star"
+          />
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default SimilarProductItem
